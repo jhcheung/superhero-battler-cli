dcc4a5bee17b453b3f5e26e74d3a117a3babffa7
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2019-10-25 01:40:17 -0400

    more ui stuff and ironed out a ton of bugs

--- lib/fighter.rb
+++ lib/fighter.rb
@@ -12,16 +12,7 @@ class Fighter < ActiveRecord::Base
     end
 
     def self.print_composite_image(f1, f2, f3)
-        # image_list = Magick::ImageList.new("./imgs/#{id1}.jpg", "./imgs/#{id2}.jpg", "./imgs/#{id3}.jpg")
-        # image_list = image_list.montage do
-        #     |image| image.tile="1x3", image.background_color = "black", self.geometry = "130x194+10+5"
-        # end
-        # image_list.write("./imgs/#{id1}_#{id2}_#{id3}.png")
-        # Catpix.print_image("./imgs/#{id1}_#{id2}_#{id3}.png", {limit_x: 0.90, limit_y: 0.90, resolution: "high"})
         image_list = Magick::ImageList.new(f1.superhero_image_dir, f2.superhero_image_dir, f3.superhero_image_dir)
-        # image_list[0]['Label'] = f1.name
-        # image_list[1]['Label'] = f2.name
-        # image_list[2]['Label'] = f3.name
         image_list = image_list.montage do
             |image| image.tile="1x3", image.background_color = "black", self.geometry = "130x194+10+5"
         end
@@ -29,24 +20,12 @@ class Fighter < ActiveRecord::Base
         Catpix.print_image("./imgs/#{f1.id}_#{f2.id}_#{f3.id}.png", {limit_x: 0.90, limit_y: 0.90, resolution: "high", center_x: true})
     end
 
-    def drafts
-        Draft.select { |draft| draft.fighter == self }
-    end
-
     def team_ids
         drafts.map { |draft| draft.team.id }
     end
 
     def wins_count #based on overall wins
-        # Battle.includes(team: [:fighters]).select do |battle| 
-        #     team_ids.include?(battle.winner_id) 
-        # end
         teams.sum(&:overall_wins_count)
-    end
-
-    def self.fighters_with_wins
-
-            Battle.wins_find_by_fighter()
     end
 
 end
